247c975acbc96adeb60a84cc658151ff36f8a835
Gleb Myznikov <myznikovgleb@yandex.ru>
2023-04-01 23:54:32 +0300

    Add mocked charts to `Dashboard` page
    * Add mocked data with categories
    * Add charts for categories to `Dashboard` page

---
+++ src/dataMocked.js
@@ -0,0 +1,26 @@
+export const categoriesMocked = [
+  {
+    name: 'Электроника',
+    example: 'Apple MacBook Pro',
+    metrics: {
+      foo: '75.0',
+      bar: '100.0'
+    }
+  },
+  {
+    name: 'Образование',
+    example: 'Advanced React with Bob Ziroll',
+    metrics: {
+      foo: '125.0',
+      bar: '110.0'
+    }
+  },
+  {
+    name: 'Медицина',
+    example: 'Курс процедур нейроседативного массаж поясничного отдела',
+    metrics: {
+      foo: '125.0',
+      bar: '50.0'
+    }
+  }
+]
