043ca9e2590ea17c6bc34ebaeb274438f2ac1dcc
alexsheks <alex.sheks.1997@gmail.com>
2023-04-02 18:54:01 +0700

    add conversion rate

---
+++ src/components/ConversionRate.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import api from '../api'
+
+export const ConversionRate = (id) => {
+  const [conversionRate, setConversionRate] = useState([])
+
+  useEffect(() => {
+    const fetchConversionRate = async () => {
+      if (id) {
+        try {
+          const response = await api.post('/conversionrate', {
+            supplier_id: id
+          })
+          setConversionRate(response.data)
+        } catch (error) {}
+      } else {
+        try {
+          const response = await api.get('/conversionrate')
+          setConversionRate(response.data)
+        } catch (error) {}
+      }
+    }
+    fetchConversionRate()
+  })
+
+  return <div>{conversionRate}</div>
+}
